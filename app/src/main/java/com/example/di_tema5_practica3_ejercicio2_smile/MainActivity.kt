0045f807d599f0aa3e71c87dd60a655c52ac19c3
Cherry708 <aluser8241@ieselcaminas.org>
2021-12-07 20:26:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/di_tema5_practica3_ejercicio2_smile/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.di_tema5_practica3_ejercicio2_smile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
